b45551ab3c06317e3bb52fb6e0999029ef51a6f9
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-04-19 20:20:15 +0000

    client file -  functions

--- client/client.c
+++ client/client.c
@@ -1,38 +1,38 @@
-/*
-    Cliente - cria socket e se conecta ao servidor
-*/
+#include "client.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-
-int main(){
-    int socket_fd; //guarda o id do socket
+int create_connection(const char *server_ip, int port) {
+    int socket_fd;
     struct sockaddr_in target;
 
-    //Criação de socket - IPv4, TCP. 0(flags)
-    socket_fd = socket (AF_INET, SOCK_STREAM, 0);
-    printf("Socket Criado\n");
+    // Criação do socket - IPv4, TCP
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+        perror("Erro ao criar o socket");
+        return -1;
+    }
+    printf("Socket criado\n");
 
     target.sin_family = AF_INET;
-    target.sin_port = htons(3001);
-    inet_aton("127.0.0.1", &(target.sin_addr));
+    target.sin_port = htons(port);
+    inet_aton(server_ip, &(target.sin_addr));
 
-    printf("Tentando conectar\n");
-    if(connect(socket_fd, (struct sockaddr*)&target, sizeof(target)) !=0){
-        printf("Problemas ao conectar\n");
-        return 0;
+    printf("Tentando conectar ao servidor %s na porta %d...\n", server_ip, port);
+    if (connect(socket_fd, (struct sockaddr *)&target, sizeof(target)) != 0) {
+        perror("Problemas ao conectar");
+        return -1;
     }
-     printf("Connectado ao servdior\n");
+    printf("Conectado ao servidor\n");
+    
+    return socket_fd;
+}
 
+void communicate_with_server(int socket_fd) {
+    // Envia mensagem "PING" para o servidor
     send(socket_fd, "PING", 5, 0);
-    printf("Escrevi uma mensagem de PING\n");
+    printf("Enviei uma mensagem de PING\n");
 
+    // Recebe a resposta do servidor
     char reply[10];
     recv(socket_fd, reply, 10, 0);
-    close(socket_fd);
-;}
+    printf("Recebi resposta do servidor: %s\n", reply);
+}
