970b4466c47e203ba2b470c31f9bfc17527bcc5e
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-04-19 20:07:32 +0000

    file header from server file implementation

---
+++ server/server.h
@@ -0,0 +1,17 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int create_server_socket();
+void bind_socket(int socket_fd);
+void listen_for_connections(int socket_fd);
+void handle_client(int connection_fd, struct sockaddr_in client);
+void close_server_socket(int socket_fd);
+
+#endif
