50590beceec355edda92d3226628a24824e41e7c
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-04-19 20:49:39 +0000

    add commands files

---
+++ client/commands.c
@@ -0,0 +1,6 @@
+/*
+    Os comandos do cliente no chat são:
+    <NOME> ---> utilizando para enviar seu nome ao servidor
+    <ALL> ---> utilizado para enviar uma mensagem ao servidor
+    <SAIR> ---> utilizado para sair da conexão
+*/
---
+++ server/commands.c
@@ -0,0 +1,5 @@
+/*
+    <ACK> ---> aceita o nome do usuário
+    <NACK> ---> não aceita o nome 
+    timeout ---> habilitar para desconectar o usuario após 60s de inatividade
+*/
