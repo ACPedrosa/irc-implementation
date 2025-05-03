41272766baeab90853a413e37f9d8ec68e36552c
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-05-03 17:24:56 +0000

    add struct for client

--- server/server.h
+++ server/server.h
@@ -8,13 +8,29 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define BUFFER_SIZE 512
+#define MAX_CLIENTES 10
+#define INATIVIDADE_TIMEOUT 60
 
+typedef struct {
+    int socket;
+    char nome[50];
+    char ip[INET_ADDRSTRLEN];
+    time_t ultimo_uso;
+} Cliente;
+
+// Funções principais do servidor
 int create_server_socket();
 void bind_socket(int socket_fd);
 void listen_for_connections(int socket_fd);
 void handle_client(int connection_fd, struct sockaddr_in client);
 void close_server_socket(int socket_fd);
+
+// Gerenciamento de cliente
+void desconectar_cliente(Cliente *cliente);
+void verificar_inatividade(Cliente *cliente);
+
 
 #endif
