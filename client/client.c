03d34b797da5ae00659366709bf72293d652a403
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-05-05 14:27:08 +0000

    update

--- client/client.c
+++ client/client.c
@@ -12,8 +12,7 @@ void *receive_messages(void *arg) {
         bytes_received = recv(socket_fd, buffer, BUFFER_SIZE - 1, 0);
         if (bytes_received > 0) {
             buffer[bytes_received] = '\0';
-            printf("Mensagem do servidor: %s\n", buffer);
-            // TODO: Processar a mensagem (atualizar a interface, etc.)
+            printf(buffer);
         } else if (bytes_received == 0) {
             printf("Servidor desconectado.\n");
             break;
