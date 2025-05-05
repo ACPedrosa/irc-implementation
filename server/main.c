18bb3a087e10259b288b575968c5e076dc447706
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-05-05 14:11:55 +0000

    add inicialização de variavies

--- server/main.c
+++ server/main.c
@@ -1,25 +1,46 @@
 #include "server.h"
 
-int socket_fd = -1; 
-pthread_t thread_conexao = 0;
+// Declaração das variáveis globais (extern)
+extern int socket_fd;
+extern pthread_t thread_conexao;
+extern pthread_t thread_inatividade;
+extern volatile sig_atomic_t servidor_ativo;
 
 void encerrar_servidor(int sinal) {
     printf("\nEncerrando o servidor...\n");
-     for (int i = 0; i < total_clientes; i++) {
-        desconectar_cliente(&clientes_conectados[i]);
+    servidor_ativo = 0; // Define a flag para parar os loops
+
+    // Cancela a thread de inatividade primeiro
+    if (thread_inatividade != 0) {
+        pthread_cancel(thread_inatividade);
+        pthread_join(thread_inatividade, NULL);
+        thread_inatividade = 0;
     }
-    if (socket_fd != -1) {
-        close_server_socket(socket_fd);
-    }
+
+    // Cancela a thread de conexão
     if (thread_conexao != 0) {
         pthread_cancel(thread_conexao);
         pthread_join(thread_conexao, NULL);
+        thread_conexao = 0;
+    }
+
+    // Desconecta todos os clientes
+     for (int i = 0; i < total_clientes; i++) {
+        desconectar_cliente(&clientes_conectados[i]);
+    }
+   
+
+    // Fecha o socket do servidor
+    if (socket_fd != -1) {
+        close_server_socket(socket_fd);
+        socket_fd = -1;
     }
     exit(0);
 }
 
 int main() {
     signal(SIGINT, encerrar_servidor);
+
     // Criação do socket
     socket_fd = create_server_socket();
     if (socket_fd < 0) {
@@ -40,7 +61,7 @@ int main() {
         close_server_socket(socket_fd);
         return 1;
     }
-    
+
     // Cria uma thread para aceitar conexões
     if (pthread_create(&thread_conexao, NULL, thread_aceita_conexoes, &socket_fd) != 0) {
         perror("Erro ao criar thread de conexões");
@@ -48,8 +69,20 @@ int main() {
         return 1;
     }
 
+    // Cria a thread para monitorar a inatividade
+    if (pthread_create(&thread_inatividade, NULL, monitorar_inatividade, NULL) != 0) {
+        perror("Erro ao criar thread de inatividade");
+        close_server_socket(socket_fd);
+        pthread_cancel(thread_conexao); // Cancela a thread de conexões
+        pthread_join(thread_conexao, NULL);
+        return 1;
+    }
+
+    // Espera a thread de conexão terminar
     pthread_join(thread_conexao, NULL);
+    pthread_join(thread_inatividade, NULL); // Espera a thread de inatividade terminar
+
+    // Fecha o socket do servidor
     close_server_socket(socket_fd);
     return 0;
 }
-
