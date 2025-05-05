00d891d1bf3963c68b1a1e34982e1177a0230e8f
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-05-05 01:27:07 +0000

    update

--- server/main.c
+++ server/main.c
@@ -1,9 +1,7 @@
 #include "server.h"
 
 int main() {
-    int socket_fd, connection_fd;
-    struct sockaddr_in client;
-    socklen_t client_size = (socklen_t)sizeof(client);
+    int socket_fd;
 
     // Criação do socket
     socket_fd = create_server_socket();
@@ -15,36 +13,14 @@ int main() {
     // Escuta
     listen_for_connections(socket_fd);
 
-    while (1) {
-        connection_fd = accept(socket_fd, (struct sockaddr*)&client, &client_size);
-        if (connection_fd < 0) {
-            perror("Erro ao aceitar conexão");
-            continue;
-        }
-
-        // Aloca memória para armazenar os dados do cliente
-        ClientData* client_data = (ClientData*)malloc(sizeof(ClientData));
-        if (client_data == NULL) {
-            perror("Erro ao alocar memória para cliente");
-            close(connection_fd);
-            continue;
-        }
-
-        // Preenche os dados do cliente
-        client_data->connection_fd = connection_fd;
-        client_data->client = client;
-
-        // Cria uma thread para tratar o cliente
-        pthread_t thread_id;
-        if (pthread_create(&thread_id, NULL, handle_client_thread, (void*)client_data) != 0) {
-            perror("Erro ao criar thread para cliente");
-            free(client_data);  // Libera a memória caso a thread não seja criada
-            close(connection_fd);
-        } else {
-            // Detach a thread para que ela seja limpa automaticamente após terminar
-            pthread_detach(thread_id);
-        }
+    // Cria uma thread para aceitar conexões
+    pthread_t thread_conexao;
+    if (pthread_create(&thread_conexao, NULL, thread_aceita_conexoes, &socket_fd) != 0) {
+        perror("Erro ao criar thread de conexões");
+        exit(1);
     }
+
+    pthread_join(thread_conexao, NULL);
 
     close_server_socket(socket_fd);
     return 0;
