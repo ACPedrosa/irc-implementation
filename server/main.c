0d2e8086d27abd64c90afa3b5464ab16ea8532a7
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-05-05 02:22:44 +0000

    update main

--- server/main.c
+++ server/main.c
@@ -6,25 +6,14 @@ int main() {
 
     // Criação do socket
     socket_fd = create_server_socket();
-    if (socket_fd < 0) {
-        fprintf(stderr, "Erro ao criar socket, encerrando servidor\n");
-        return 1;
-    }
+    if (socket_fd < 0) return 0;
 
     // Bind
-    if (bind_socket(socket_fd) != 0) {
-        fprintf(stderr, "Erro ao fazer bind do socket, encerrando servidor\n");
-        close_server_socket(socket_fd);
-        return 1;
-    }
+    bind_socket(socket_fd);
 
     // Escuta
-    if (listen_for_connections(socket_fd) != 0) {
-        fprintf(stderr, "Erro ao escutar na porta, encerrando servidor\n");
-        close_server_socket(socket_fd);
-        return 1;
-    }
-
+    listen_for_connections(socket_fd);
+    
     // Cria uma thread para aceitar conexões
     if (pthread_create(&thread_conexao, NULL, thread_aceita_conexoes, &socket_fd) != 0) {
         perror("Erro ao criar thread de conexões");
@@ -32,7 +21,6 @@ int main() {
         return 1;
     }
 
-    // Espera a thread de conexão terminar (ou ser cancelada)
     pthread_join(thread_conexao, NULL);
 
     close_server_socket(socket_fd);
